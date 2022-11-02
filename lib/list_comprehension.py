4bebef8587b6b311436f467c83760f727d5b7f5c
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-11-02 14:23:53 -0700

    tests passing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+# LIST COMPREHENSION
+# new_list = [optional_operation(item) for item in old_list if optional_condition == True]
+
+# GENERATOR EXPRESSIONS
+# generator_object = (optional_operation(item) for item in old_list)
+#### used for big datasets #####
+
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
